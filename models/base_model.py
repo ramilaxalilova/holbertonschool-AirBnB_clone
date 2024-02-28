9265fe5a9aca32b004edf90e510eb0885056f442
Rashadmlkv <resadmlkv@gmail.com>
2024-02-28 11:08:15 +0000

    upload

--- models/base_model.py
+++ models/base_model.py
@@ -24,14 +24,14 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.__init__.storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
         return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         self.updated_at = datetime.now()
-        models.__init__.storage.save()
+        models.storage.save()
         return self.updated_at
 
     def to_dict(self):
