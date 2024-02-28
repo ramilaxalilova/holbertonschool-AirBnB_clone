2b7f8b05962d1fb0ceef6e303a02de9299f61314
unknown <xalilovaramila@yahooo.com>
2024-02-28 07:30:24 +0400

    updateconsole

---
+++ console.py
@@ -0,0 +1,24 @@
+import cmd
+from models.base_model import BaseModel
+from models import storage
+
+class HBNBCommand(cmd.Cmd):
+    """Command interpreter for the HBNB console."""
+    
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program."""
+        return True
+
+    def do_EOF(self, arg):
+        """Exit the program when EOF (Ctrl+D) is reached."""
+        print("")
+        return True
+
+    def emptyline(self):
+        """Do nothing when an empty line is entered."""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
