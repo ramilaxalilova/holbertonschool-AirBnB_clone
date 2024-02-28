2f657e28164a2383cb5e180962fbe956bcc612c0
Rashadmlkv <resadmlkv@gmail.com>
2024-02-28 11:12:10 +0000

    upload

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from uuid import uuid4
 from datetime import datetime
-import models
+from models import storage
 import json
 """
     Base class
@@ -24,14 +24,14 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+            storage.new(self)
 
     def __str__(self):
         return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
         return self.updated_at
 
     def to_dict(self):
