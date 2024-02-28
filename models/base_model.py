f2e825966fe52155564e5cef5b7ea852527c8eb1
Rashadmlkv <resadmlkv@gmail.com>
2024-02-28 20:27:50 +0000

    circular imp

--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,7 @@ class BaseModel:
     """ Class """
 
     def __init__(self, *args, **kwargs):
-        import models
+        from . import storage
         if (kwargs != {}):
             for key, value in kwargs.items():
                 if key == 'id':
@@ -23,15 +23,15 @@ class BaseModel:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            models.storage.new(self)
+            storage.new(self)
 
     def __str__(self):
         return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        import models
+        from . import storage
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.save()
 
     def to_dict(self):
         newdict = self.__dict__.copy()
