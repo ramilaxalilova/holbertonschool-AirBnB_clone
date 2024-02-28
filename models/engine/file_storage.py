29b03b0abc71ca939a11ef0c455c0cf577526753
Rashadmlkv <resadmlkv@gmail.com>
2024-02-28 20:31:33 +0000

    circular imp

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -25,13 +25,13 @@ class FileStorage:
             json.dump(new_dict, f)
 
     def reload(self):
-        from models.base_model import BaseModel
+        import models.base_model
 
         try:
             with open(self.__file_path, "r") as file_path:
                 objdict = json.load(file_path).items()
                 for k, v in objdict:
-                    obje = BaseModel(**v)
+                    obje = models.base_model.BaseModel(**v)
                     ###v  = eval(k.split(".")[0])(**v)
                     self.__objects[k] = obje
         except Exception:
