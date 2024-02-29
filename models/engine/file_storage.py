ce73e5fe062931b41d8db6f196473a94f2439f95
Rashadmlkv <resadmlkv@gmail.com>
2024-02-29 01:28:18 +0000

    5th task

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -6,7 +6,7 @@ from models import base_model
 
 
 class FileStorage:
-    """ 
+    """
         file_path is file's name
         objects is dictionary
     """
@@ -22,8 +22,7 @@ class FileStorage:
     def new(self, obj):
         """ sets in dictionary in dict """
 
-        key = f"{type(obj).__name__}.{obj.id}"
-        FileStorage.__objects[key] = obj
+        FileStorage.__objects[f"{type(obj).__name__}.{obj.id}"] = obj
 
     def save(self):
         """ save to file """
@@ -31,7 +30,7 @@ class FileStorage:
         dct1 = {}
         for key, value in FileStorage.__objects.items():
             dct1[key] = value.to_dict()
-        with open(FileStorage.__file_path, 'a') as write:
+        with open(FileStorage.__file_path, 'w') as write:
             json.dump(dct1, write)
 
     def reload(self):
@@ -39,6 +38,8 @@ class FileStorage:
 
         try:
             with open(FileStorage.__file_path, "r") as read:
-                json.load(FileStorage.__objects, read) 
+                tmp = json.load(read)
+            for key, value in tmp.items():
+                FileStorage.__objects[key] = base_model.BaseModel(**tmp[key])
         except Exception:
             pass
